8b713c9e658724ff1875e9ab6b52a12e797b0d1f
Adrián E. Córdoba <software.asia@gmail.com>
2021-02-12 13:53:24 -0300

    Popups removed.

--- workspace/filesbackup.py
+++ workspace/filesbackup.py
@@ -3,11 +3,9 @@
 """Application for check the files status and guard them from a server."""
 import shutil
 import os
-import tkinter as tk
 from datetime import datetime, timedelta
 from ftplib import FTP
 from pathlib import Path
-from tkinter import ttk
 from pytz import timezone
 
 import config
@@ -15,43 +13,44 @@ from file import File
 
 DAYS_OF_WEEK = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
 
+
 def main():
-    ftp = FTP(config.server['ip'])
-    ftp.login(user=config.server['username'], passwd=config.server['password'])
+    try:
+        ftp = FTP(config.server['ip'])
+        ftp.login(user=config.server['username'],
+                  passwd=config.server['password'])
 
-    now = datetime.now(timezone('America/Argentina/Buenos_Aires'))
-    print(f'{now}: Backup in progress...')
-    files = []
+        now = datetime.now(timezone('America/Argentina/Buenos_Aires'))
+        print(f'{now}: Backup in progress...')
+        files = []
 
-    for filename in config.filenames:
-        file = File(filename, ftp.size(filename), datetime.strptime(ftp.sendcmd('MDTM ' + filename)[4:], "%Y%m%d%H%M%S"))
-        files.append(file)
+        for filename in config.filenames:
+            file = File(filename, ftp.size(filename), datetime.strptime(
+                ftp.sendcmd('MDTM ' + filename)[4:], "%Y%m%d%H%M%S"))
+            files.append(file)
 
-    for file in files:
-        if (file.modification_time + timedelta(days=1)) < now:
-            print(f'{file.filename} file not modified in last 24 hours. Last modification: {file.modification_time}.')
-            popup_message("WARNING!", f'{file.filename} file not modified in last 24 hours.\nLast modification: {file.modification_time}.')
-        else:
-            localfile = open(file.filename, 'wb')
-            ftp.retrbinary('RETR ' + file.filename, localfile.write, 1024)
-            localfile.close()
-            if file.size == os.path.getsize(file.filename):
-                shutil.move(file.filename, f'/backup/sgi_backup/{DAYS_OF_WEEK[file.modification_time.weekday()]}/{file.filename}')
-                print(f'Successfully downloaded {file.filename} ({file.size} bytes).')
+        for file in files:
+            if (file.modification_time + timedelta(days=1)) < now:
+                print(
+                    f'{file.filename} file not modified in last 24 hours. Last modification: {file.modification_time}.')
             else:
-                popup_message("WARNING!", f'{file.filename} file couldn\'t be downloaded.\nOriginal file size: {file.size}.\nDownloaded file size: {os.path.getsize(file.filename)}')
+                localfile = open(file.filename, 'wb')
+                ftp.retrbinary('RETR ' + file.filename, localfile.write, 1024)
+                localfile.close()
+                if file.size == os.path.getsize(file.filename):
+                    print(
+                        f'Successfully downloaded {file.filename} ({file.size} bytes).')
+                    shutil.move(
+                        file.filename, f'/backup/sgi_backup/{DAYS_OF_WEEK[file.modification_time.weekday()]}/{file.filename}')
+                    print(
+                        f'{file.filename} successfully moved to /backup/sgi_backup/{DAYS_OF_WEEK[file.modification_time.weekday()]}/.')
+                else:
+                    print(f'{file.filename} file couldn\'t be downloaded.\n\tOriginal file size: {file.size}.\n\tDownloaded file size: {os.path.getsize(file.filename)}')
 
-    ftp.quit()
+        ftp.quit()
 
-
-def popup_message(title, message):
-    popup = tk.Tk()
-    popup.wm_title(title)
-    label = ttk.Label(popup, text=message, font=("Helvetica", 10))
-    label.pack(side="top", fill="x", pady=10)
-    B1 = ttk.Button(popup, text="Ok", command = popup.destroy)
-    B1.pack()
-    popup.mainloop()
+    except ftplib.all_errors as e:
+        print("FTP error:", e)
 
 
 # call main().
