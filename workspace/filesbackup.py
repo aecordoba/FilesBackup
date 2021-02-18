c317a3df4bda835cdd385614dc285571db6bd62f
Adrián E. Córdoba <software.asia@gmail.com>
2021-02-18 20:39:20 -0300

    Code formatted.

--- workspace/filesbackup.py
+++ workspace/filesbackup.py
@@ -2,12 +2,13 @@
 # filesbackup.py
 """Application for check the files status and guard them from a server."""
 
-import shutil
-import os
 import ftplib
+import os
+import shutil
 from datetime import datetime, timedelta
 from ftplib import FTP
 from pathlib import Path
+
 from pytz import timezone
 
 import config
@@ -19,25 +20,29 @@ DAYS_OF_WEEK = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
 def main():
     try:
         ftp = FTP(config.server['ip'])
-        ftp.login(user=config.server['username'], passwd=config.server['password'])
+        ftp.login(user=config.server['username'],
+                  passwd=config.server['password'])
 
         now = datetime.now(timezone('America/Argentina/Buenos_Aires'))
         print(f'{now}: Backup in progress...')
         files = []
 
         for filename in config.filenames:
-            file = File(filename, ftp.size(filename), datetime.strptime(ftp.sendcmd('MDTM ' + filename)[4:], "%Y%m%d%H%M%S"))
+            file = File(filename, ftp.size(filename), datetime.strptime(
+                ftp.sendcmd('MDTM ' + filename)[4:], "%Y%m%d%H%M%S"))
             files.append(file)
 
         for file in files:
             if (file.modification_time + timedelta(days=1)) < now:
-                print(f'{file.filename} file not modified in last 24 hours. Last modification: {file.modification_time}.')
+                print(
+                    f'{file.filename} file not modified in last 24 hours. Last modification: {file.modification_time}.')
             else:
                 localfile = open(file.filename, 'wb')
                 ftp.retrbinary('RETR ' + file.filename, localfile.write, 1024)
                 localfile.close()
                 if file.size == os.path.getsize(file.filename):
-                    print(f'Successfully downloaded {file.filename} ({file.size} bytes).')
+                    print(
+                        f'Successfully downloaded {file.filename} ({file.size} bytes).')
 
                     destination = config.base_directory
                     if file.modification_time.day == 1:
@@ -45,7 +50,8 @@ def main():
                     elif file.modification_time.day == 15:
                         destination += "15th/"
                     else:
-                        destination += DAYS_OF_WEEK[file.modification_time.weekday()] + "/"
+                        destination += DAYS_OF_WEEK[file.modification_time.weekday()] + \
+                            "/"
 
                     shutil.move(file.filename, f'{destination}{file.filename}')
                     print(f'{file.filename} successfully moved to {destination}.')
