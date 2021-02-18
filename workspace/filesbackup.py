98c069ac92c41d5ee7d4d673e996dcd2b1555a42
Adrián E. Córdoba <software.asia@gmail.com>
2021-02-18 14:04:57 -0300

    Days 1st and 15th added.

--- workspace/filesbackup.py
+++ workspace/filesbackup.py
@@ -19,16 +19,14 @@ DAYS_OF_WEEK = ["MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"]
 def main():
     try:
         ftp = FTP(config.server['ip'])
-        ftp.login(user=config.server['username'],
-                  passwd=config.server['password'])
+        ftp.login(user=config.server['username'], passwd=config.server['password'])
 
         now = datetime.now(timezone('America/Argentina/Buenos_Aires'))
         print(f'{now}: Backup in progress...')
         files = []
 
         for filename in config.filenames:
-            file = File(filename, ftp.size(filename), datetime.strptime(
-                ftp.sendcmd('MDTM ' + filename)[4:], "%Y%m%d%H%M%S"))
+            file = File(filename, ftp.size(filename), datetime.strptime(ftp.sendcmd('MDTM ' + filename)[4:], "%Y%m%d%H%M%S"))
             files.append(file)
 
         for file in files:
@@ -40,9 +38,17 @@ def main():
                 localfile.close()
                 if file.size == os.path.getsize(file.filename):
                     print(f'Successfully downloaded {file.filename} ({file.size} bytes).')
-                    shutil.move(
-                        file.filename, f'{config.base_directory}{DAYS_OF_WEEK[file.modification_time.weekday()]}/{file.filename}')
-                    print(f'{file.filename} successfully moved to {config.base_directory}{DAYS_OF_WEEK[file.modification_time.weekday()]}/.')
+
+                    destination = config.base_directory
+                    if file.modification_time.day == 1:
+                        destination += "1st/"
+                    elif file.modification_time.day == 15:
+                        destination += "15th/"
+                    else:
+                        destination += DAYS_OF_WEEK[file.modification_time.weekday()] + "/"
+
+                    shutil.move(file.filename, f'{destination}{file.filename}')
+                    print(f'{file.filename} successfully moved to {destination}.')
                 else:
                     print(f'{file.filename} file couldn\'t be downloaded.\n\tOriginal file size: {file.size}.\n\tDownloaded file size: {os.path.getsize(file.filename)}')
 
