00fc5edbb3f8d079ab8fa4fe931cdbfea7c8be69
Adrián E. Córdoba <software.asia@gmail.com>
2022-07-27 14:42:06 -0300

    Version 2.0

--- filesbackup.py
+++ filesbackup.py
@@ -2,11 +2,10 @@
 # filesbackup.py
 """Application for check the files status and guard them from a server."""
 
-import ftplib
 import os
 import shutil
 from datetime import datetime, timedelta
-from ftplib import FTP
+from ftplib import FTP, all_errors
 from pytz import timezone
 import config
 from file import File
@@ -21,7 +20,7 @@ def main():
         ftp.login(user=config.server['username'],
             passwd=config.server['password'])
 
-        print(f'{now}: Backup in progress...')
+        print(now, ": Backup in progress...", sep='')
         files = []
 
         for filename in config.filenames:
@@ -31,37 +30,33 @@ def main():
 
         for file in files:
             if (file.modification_time + timedelta(days=1)) < now:
-                print(
-                    f'{file.filename} file not modified in last 24 hours. Last modification: {file.modification_time}.')
+                print(file.filename, "file not modified in last 24 hours. Last modification:", file.modification_time, ".")
             else:
-                print(f'{file} was found.')
+                print(file.filename, " (size: ", file.size, " last modification: ", file.modification_time, ") was found.", sep='')
                 localfile = open(file.filename, 'wb')
                 ftp.retrbinary('RETR ' + file.filename, localfile.write, 1024)
                 localfile.close()
-                if file.size==os.path.getsize(file.filename):
-                    print(
-                        f'Successfully downloaded {file.filename} ({file.size} bytes).')
+                if file.size == os.path.getsize(file.filename):
+                    print("Successfully downloaded ", file.filename, " (", file.size, " bytes).", sep='')
 
                     destination = config.base_directory
-                    if file.modification_time.day==1:
-                        destination += "1st/"
-                    elif file.modification_time.day==15:
-                        destination += "15th/"
+                    if file.modification_time.day == 1 or file.modification_time.day == 15:
+                        destination += "yearly/" + file.modification_time.strftime("%B")[0:3].lower() + file.modification_time.strftime("%d") + "/"
                     else:
                         destination += DAYS_OF_WEEK[file.modification_time.weekday()] + \
                                        "/"
 
-                    shutil.move(file.filename, f'{destination}{file.filename}')
-                    print(f'{file.filename} successfully moved to {destination}.')
+                    shutil.move(file.filename, destination + file.filename)
+                    print(file.filename, " successfully moved to ", destination, ".", sep='')
                 else:
-                    print(f'{file.filename} file couldn\'t be downloaded.\n\tOriginal file size: {file.size}.\n\tDownloaded file size: {os.path.getsize(file.filename)}')
+                    print(file.filename, "file could not be downloaded. Original file size:", file.size, "Downloaded file size:", os.path.getsize(file.filename))
 
         ftp.quit()
 
-    except ftplib.all_errors as e:
-        print(f'{now}: FTP error:', e)
+    except all_errors as e:
+        print(now, ": FTP error: ", e, sep='')
 
 
 # call main().
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
